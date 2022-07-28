a5cdf9218b9acb003792a4955b2f5755e5560c94
Devil Dev <84636715+MAPXOR12@users.noreply.github.com>
2022-07-28 11:45:32 +0300

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
    
     "token": "Nzk4NDI2MzQwNzM0MjcxNDg4.Gw4L6k.4makd3Dg1q4eMuSRg5a3mNt6xLhBo9fGcZqUJw", // required, https://discordjs.guide/preparations/setting-up-a-bot-application.html#your-token
     "prefix": ".", // required, command prefix
-    "mongooseConnectionString": "", // optional.
+    "mongooseConnectionString": "mongodb+srv://c4clan1:c4clan1@c4clan.ohxsoxt.mongodb.net/?retryWrites=true&w=majority", // optional.
     "embedcolor": "#118fff",
     "guildid": "872563022344892426", // required, after new update for slash-commands 
     "OWNER_ID": "743887896481628190", // required, for owner-only commands
