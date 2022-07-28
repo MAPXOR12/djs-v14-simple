3c0200c85373129bd2ccf263ccee55a440ef6b5f
Devil Dev <84636715+MAPXOR12@users.noreply.github.com>
2022-07-28 12:05:57 +0300

    Update index.js

--- index.js
+++ index.js
@@ -31,6 +31,41 @@ module.exports = client;
 client.commands = new Collection();
 client.slashcommand = new Collection();
 
+//PACK
+const { Client, Collection, GatewayIntentBits, Partials } = require("discord.js");
+
+const client = new Client({
+  disableEveryone: true,
+  shards: "auto",
+  disableMentions: "everyone",
+  partials: [Partials.Channel, Partials.Message, Partials.User, Partials.GuildMember, Partials.Reaction],
+  intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.GuildIntegrations,
+        GatewayIntentBits.MessageContent,
+        GatewayIntentBits.GuildWebhooks,
+        GatewayIntentBits.GuildInvites,
+        GatewayIntentBits.GuildVoiceStates,
+        GatewayIntentBits.GuildPresences,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.GuildMessageReactions,
+        GatewayIntentBits.GuildMessageTyping,
+        GatewayIntentBits.DirectMessages,
+        GatewayIntentBits.DirectMessageReactions,
+        GatewayIntentBits.DirectMessageTyping,
+
+      ],
+});
+
+module.exports = client;
+
+// Global Variables
+client.commands = new Collection();
+client.slashcommand = new Collection();
+
+this.scategories = fs.readdirSync("./commands/slash");
+
 client.config = require("./config.js");
 
 const mongoose = require("mongoose");
