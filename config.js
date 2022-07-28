d1911897789c95e634ccf6f18f2425b10879b881
Devil Dev <84636715+MAPXOR12@users.noreply.github.com>
2022-07-28 11:38:36 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 module.exports = {
    
-    "token": "", // required, https://discordjs.guide/preparations/setting-up-a-bot-application.html#your-token
+    "token": "Nzk4NDI2MzQwNzM0MjcxNDg4.Gw4L6k.4makd3Dg1q4eMuSRg5a3mNt6xLhBo9fGcZqUJw", // required, https://discordjs.guide/preparations/setting-up-a-bot-application.html#your-token
     "prefix": ".", // required, command prefix
     "mongooseConnectionString": "", // optional.
     "embedcolor": "#118fff",
-    "guildid": "", // required, after new update for slash-commands 
-    "OWNER_ID": "", // required, for owner-only commands
+    "guildid": "872563022344892426", // required, after new update for slash-commands 
+    "OWNER_ID": "743887896481628190", // required, for owner-only commands
 };
